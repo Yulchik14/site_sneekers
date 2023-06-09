f111f305eca4238ce939ed60faf8603e77f75c7a
Zloyyurann <77922325+Zloyyurann@users.noreply.github.com>
2023-06-09 12:09:38 +0300

    Add files via upload

--- routes.py
+++ routes.py
@@ -9,30 +9,12 @@ def index():
     items = Item.query.all()
     return render_template("index.html", items = items)
 
-@app.route("/faqs")
-def faqs():
-    return render_template("faqs.html")
+@app.route("/item/<id>")
+def item(id):
+    item = Item.query.get(id)
+    items = Item.query.filter_by(category = item.category).all()
+    return render_template("item.html", item = item, items = items)
     
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-@app.route("/checkout")
-def checkout():
-    return render_template("checkout.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/productdetail")
-def productdetail():
-    return render_template("productdetail.html")
-
-@app.route("/products")
-def products():
-    return render_template("products.html")
-
-@app.route("/shoppingcart")
-def shoppingcart():
-    return render_template("shoppingcart.html")
